11ca873aff3e60cf4f2698fa27a45cfcfe4a11ea
Isaac Wolford <isaacmwolford@gmail.com>
2026-04-08 02:45:38 -0400

    Added rendering range support to the std Output plugin. This also involved improvements to other areas in the codebase.

--- src/studio/stdplgns/output.py
+++ src/studio/stdplgns/output.py
@@ -8,12 +8,17 @@ from textual.widgets.selection_list import Selection
 
 # Standard Imports
 from typing import Iterator
+from core.numlib import INF, str_to_num, is_infinity
+from core.engine import Event as FlowEvent
 from studio.model import Plugin, FlowLangBase
 
 
 class P(Plugin):
     def on_initialized(self) -> None:
         self.name = 'output'
+
+        # attributes
+        self._render_range: tuple[int, int] = (-100, INF)
 
         # connect model signals
         self.model.active_flow.flow.on_evolved_n.connect(self.on_evolved)
@@ -22,8 +27,13 @@ class P(Plugin):
 
         # connect view signals
         self.view.sig_checkbox_changed.connect(self.handle_checkbox)
+        self.view.sig_input_submit.connect(self.handle_input_submit)
 
     def controls(self) -> Iterator[Widget]:
+        self.render_range = Input(value='-100:', id='render-limit')
+        self.render_range.border_title = 'Render Range'
+        yield self.render_range
+
         with Collapsible(title='Column Controls', collapsed=False):
             self.hidden_space_columns = Input()
             self.hidden_space_columns.border_title = 'Hidden spaces'
@@ -59,8 +69,22 @@ class P(Plugin):
 
         yield Label()
 
-    def handle_checkbox(self, sig: Checkbox.Changed) -> None:
+    def handle_checkbox(self, e: Checkbox.Changed) -> None:
         pass
+
+    def handle_input_submit(self, e: Input.Submitted):
+        _id: str = e.input.id
+        if _id == 'render-limit':
+            try:
+                rs: list[str] = e.value.strip().split(':')
+                self._render_range = (
+                    str_to_num(rs[0]) if rs[0] else 0,
+                    str_to_num(rs[1]) if rs[1] else INF
+                )
+                self._update_rows(self.model.active_flow.flow)
+            except:
+                self.view.notify('Invalid render range', severity='error')
+                e.input.value = '{0}:{1}'.format(*self._render_range)
 
     def panel(self) -> TabPane | None:
         self.data_table = DataTable(id='data-table')
@@ -78,30 +102,43 @@ class P(Plugin):
             self.data_table
         )
 
-    def on_evolved(self, f: FlowLangBase, steps: int) -> None:
-        cft = self.cft
-        add_row = self.data_table.add_row
-        if self.data_table.row_count == 0:
-            steps += 1  # to include the first space state
-        for event in f.events[-steps:]:
-            cft(  # we are potentially calling from thread, thus this.
-                add_row,
-                event.time,
-                event.causal_distance_to_creation,
-                tuple(event.causally_connected_events),
-                str(event.spaces.__next__())
-                .replace('A', '[on blue3] A [/on blue3]')
-                .replace('B', '[on magenta] B [/on magenta]')
-                .replace('C', '[on yellow] C [/on yellow]'),
-                key=str(event.time)
-            )
+    def _add_row(self, event: FlowEvent):
+        self.data_table.add_row(  # maybe make a separate add_row()
+            event.time,
+            event.causal_distance_to_creation,
+            tuple(event.causally_connected_events),
+            str(event.spaces.__next__())
+            .replace('A', '[on blue3] A [/on blue3]')
+            .replace('B', '[on magenta] B [/on magenta]')
+            .replace('C', '[on yellow] C [/on yellow]'),
+            key=str(event.time)
+        )
 
-        # if after clearing, we need to update the column width... not that costly.
+    def _update_rows(self, f: FlowLangBase) -> None:
+        a, b = self._render_range
+        self.on_clear()
+        for event in f.events[a:b + (1 if b > 0 else 0)]:
+            self._add_row(event)
         self._refresh_column_widths()
 
-        # if evolving 1 step, scroll to end
-        if steps == 1:
-            self.data_table.scroll_end(animate=False)
+    def on_evolved(self, f: FlowLangBase, steps: int) -> None:
+        dt = self.data_table
+        if not dt.row_count:
+            steps += 1  # to include the first space state
+        flush_mode: bool = self._render_range[0] < 0 and is_infinity(self._render_range[1])
+        render_limit: int = abs(self._render_range[0])  # only used when flush mode is true
+        if flush_mode and steps >= render_limit:
+            self._update_rows(f)
+        else:
+            short_circuit: bool = False  # just a little optimization for large loops
+            for event in f.events[-steps:]:
+                if short_circuit or flush_mode and dt.row_count >= render_limit:
+                    short_circuit = True
+                    self.cft(lambda: dt.remove_row(dt.coordinate_to_cell_key(Coordinate(0, 0)).row_key))
+                self.cft(self._add_row, event)
+        self._refresh_column_widths()
+        if not flush_mode:
+            dt.scroll_end(animate=False)
 
     def on_undo(self, f: FlowLangBase, steps: int) -> None:
         cft = self.cft
